4cf97b91664f824c6b4f2663b15214492919efc6
basudeen <basudeen55@gmail.com>
2026-01-08 18:30:01 +0530

    task get api updated

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,5 +1,6 @@
 const { ObjectId } = require('bson');
 const task = require('./../models/task_Schema');
+const dayjs = require('dayjs');
 const { default: mongoose } = require('mongoose');
 
 module.exports = {
@@ -29,8 +30,20 @@ module.exports = {
     },
     GetTasks: async (req, res) => {
         try {
-            const createtask = await task.find();
-            if (!createtask) {
+            let { search, status } = req.query;
+            let filter = {};
+            if (search) {
+                filter.title = {
+                    $regex: search,
+                    $options: 'i'
+                }
+            }
+            if (status) {
+                filter.status = status;
+            }
+     
+            const gettask = await task.find(filter);
+            if (!gettask) {
                 res.status(400).json({
                     success: false,
                     message: 'task detail not fetched successfully..!',
@@ -41,7 +54,40 @@ module.exports = {
                 res.status(200).json({
                     success: true,
                     message: 'task detail fetched successfully..!',
-                    data: createtask
+                    data: gettask
+                })
+            }
+        }
+        catch (error) {
+            res.status(500).json({
+                success: false,
+                message: 'Internal Server Error',
+                error: error.message
+            })
+        }
+    },
+    GetTaskID: async (req, res) => {
+        try {
+            let { id } = req.params;
+            const gettask = await task.findById(id, { title: 1, description: 1, status: 1, _id: 0, createdAt: 1, updatedAt: 1 }).lean();
+
+            if (!gettask) {
+                res.status(400).json({
+                    success: false,
+                    message: 'task detail not fetched successfully..!',
+                    data: []
+                })
+            }
+            else {
+
+                gettask.createdAt = dayjs(gettask.createdAt).format("DD-MM-YYYY");
+                gettask.updatedAt = dayjs(gettask.updatedAt).format("DD-MM-YYYY");
+
+                console.log(gettask.createdAt);
+                res.status(200).json({
+                    success: true,
+                    message: 'task detail fetched successfully..!',
+                    data: gettask
                 })
             }
         }
@@ -105,8 +151,8 @@ module.exports = {
             if (!deletetask) {
                 return res.status(400).json({ success: true, message: 'Task not found' });
             }
-            else{
-                res.status(200).json({success:true,message:'Task Removed Successfully..'})
+            else {
+                res.status(200).json({ success: true, message: 'Task removed successfully..' })
             }
         }
         catch (error) {
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const taskController = require('./../controllers/task');
 router.post('/task', taskController.Createtask);
 router.get('/task', taskController.GetTasks);
-router.put('/task/:id', taskController.UpdateTask);
+router.get('/task/:id', taskController.GetTaskID);
+router.patch('/task/:id', taskController.UpdateTask);
 router.delete('/task/:id', taskController.DeleteTask);
 module.exports = router;
